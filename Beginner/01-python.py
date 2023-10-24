787c5962e4fb768a6a7168f49a78992270c4fde8
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2023-10-24 19:34:25 +0530

    updated some files

--- Beginner/01-python.py
+++ Beginner/01-python.py
@@ -1,14 +1,29 @@
-name =  input("Enter Your name : ");
-print('Your name is : '+name)
+# name =  input("Enter Your name : ");
+# print('Your name is : '+name)
 
-age = int(input("Your Age : "))
-print('You are '+str(age)+' years old ')
+# age = int(input("Your Age : "))
+# print('You are '+str(age)+' years old ')
 
-if(age>=18):
-    print('You can vote :)')
-else:
-    print("You can't vote sorry :(")
+# if(age>=18):
+#     print('You can vote :)')
+# else:
+#     print("You can't vote sorry :(")
 
 
-print(type(name))
-print(type(age))
+# print(type(name))
+# print(type(age))
+
+# --------------------------------------------
+
+number = int(input("Enter the length of the string : "))
+
+array = []
+
+for i in range(number):
+    someNumber = int(input("Enter number : "))
+    if(someNumber<10):
+        array.append(someNumber)
+    else:
+        print('number greater than 10')
+        
+print(array)
