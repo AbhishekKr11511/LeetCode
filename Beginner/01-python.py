28df25cf2dc1c6c69b61b570a994d049da684fb2
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2023-10-24 19:22:24 +0530

    just added a folder

---
+++ Beginner/01-python.py
@@ -0,0 +1,14 @@
+name =  input("Enter Your name : ");
+print('Your name is : '+name)
+
+age = int(input("Your Age : "))
+print('You are '+str(age)+' years old ')
+
+if(age>=18):
+    print('You can vote :)')
+else:
+    print("You can't vote sorry :(")
+
+
+print(type(name))
+print(type(age))
