2cc68348ebd85f00cc8a20ec2424b308d14c0bbe
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2023-12-03 01:39:04 +0530

    More question

--- NeetCode 150/Bit Manipulation/7. Reverse Integer.py
+++ NeetCode 150/Bit Manipulation/7. Reverse Integer.py
@@ -1,12 +1,26 @@
-print(2**31)
+
 
 def reverse(x):
-    i = 1
+    i = 0
     sum = 0
+    isneg = False
+    if x < 0: isneg=True
+    x = abs(x)
     digitArray= []
     while x:
         digitArray.append(x % 10)
         x //= 10
-    print(digitArray)
+        i += 1
+    j = 0
+    while i>0:
+        sum += digitArray[j] * (10**(i-1))
+        i -= 1
+        j += 1
+    if sum < -2**31 or sum > 2**31:
+        return 0
+    elif isneg:
+        return -sum
+    else:
+        return sum
 
-print(reverse(123))
+print(reverse(120))
