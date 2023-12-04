2545162f5c12503f15a4b389af1a65abd113c114
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2023-12-04 23:38:24 +0530

    Stack pretty much completee

--- NeetCode 150/Stack/84. Largest Rectangle in Histogram.py
+++ NeetCode 150/Stack/84. Largest Rectangle in Histogram.py
@@ -17,5 +17,22 @@ Output: 4
 
 def largestRectangleArea(heights):
     stack = []
+    maxArea = 0
     for i,h in enumerate(heights):
-        
+        j = 0
+        while stack and stack[-1][1] > h:
+            topIdx , topHeight = stack.pop()
+            maxArea = max(maxArea, topHeight * (i-topIdx+j), h*(i-topIdx))
+            i = topIdx
+            j+=1
+        stack.append([i,h])
+    
+    finalIndex = len(heights)
+    # while stack:
+    #     maxArea = max(maxArea, stack[-1][1] * (finalIndex - stack[-1][0]))
+    #     stack.pop()
+    for i, h in enumerate(stack):
+        maxArea = max(maxArea, h[1]*(len(heights)-h[0]))
+    return maxArea
+
+print(largestRectangleArea([6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3]))
