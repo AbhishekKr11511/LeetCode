78d449cbcda8873e3914f8cd2fd7776ba9826b72
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2023-12-05 21:22:40 +0530

    some changes made

--- NeetCode 150/Binary Search/981. Time Based Key-Value Store.py
+++ NeetCode 150/Binary Search/981. Time Based Key-Value Store.py
@@ -25,3 +25,42 @@ timeMap.get("foo", 4);         // return "bar2"
 timeMap.get("foo", 5);         // return "bar2"
 '''
 
+class TimeMap(object):
+
+    def __init__(self):
+        self.time_map = {}
+
+    def set(self, key, value, timestamp):
+        if key in self.time_map:
+            self.time_map.get(key)[timestamp] = value
+            return
+        self.time_map[key] = {timestamp: value}
+        
+
+    def get(self, key, timestamp):
+        if key in self.time_map:
+            self.keyMap = self.time_map[key]
+            while timestamp > 0:
+                if timestamp in self.keyMap:
+                    return self.keyMap[timestamp]
+                else:
+                    timestamp -= 1
+        return ""
+    
+    def display(self):
+        print(self.time_map)
+
+
+timeMap = TimeMap()
+timeMap.set("foo", "bar", 1)
+timeMap.set("foo", "bar2", 4)
+
+timeMap.display()
+
+# print(timeMap.get("foo", 1))
+print(timeMap.get("foo", 3))
+print(timeMap.get("foo", 4))
+print(timeMap.get("foo", 5))
+# timeMap.get("foo", 3)
+# timeMap.get("foo", 4)
+# timeMap.get("foo", 5)
