415b04f1a148a2bb81c404b1c15f6897662baa0b
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2023-12-05 22:08:43 +0530

    Made final commit of today 5 DEC

--- NeetCode 150/Binary Search/4. Median of Two Sorted Arrays.py
+++ NeetCode 150/Binary Search/4. Median of Two Sorted Arrays.py
@@ -14,3 +14,31 @@ Input: nums1 = [1,2], nums2 = [3,4]
 Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 '''
+def findMedianSortedArrays(nums1, nums2):
+    fullList = []
+    i = 0
+    j = 0
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i]<nums2[j]:
+            fullList.append(nums1[i])
+            i += 1
+        else:
+            fullList.append(nums2[j])
+            j += 1
+    while j < len(nums2):
+        fullList.append(nums2[j])
+        j += 1
+    while i < len(nums1):
+        fullList.append(nums1[i])
+        i += 1
+    print(fullList)
+    fulllength = len(fullList)
+    if fulllength%2 == 0:
+        median = (fullList[fulllength//2 - 1] + fullList[fulllength//2])/2
+        return median
+    else:
+        median = fullList[fulllength//2]
+        return median
+
+
+print(findMedianSortedArrays([1,2], [3,4]))
