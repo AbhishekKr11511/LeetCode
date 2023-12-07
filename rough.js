7fbd816495e9d070ed896ed2bdacedd89aa84d27
Abhishek Kumar <abhishek.kr.11511@gmail.com>
2023-12-07 11:46:08 +0530

    Some rough work for hackerEarth

--- rough.js
+++ rough.js
@@ -1,3 +1,15 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+var stdin_input = "";
+
+process.stdin.on("data", function (input) {
+    stdin_input += input;                               // Reading input from STDIN
+});
+
+process.stdin.on("end", function () {
+   main(stdin_input);
+});
+
 function check(num){
     let factors = []
     for(let i=1; i<= num/2;i++){
@@ -5,11 +17,28 @@ function check(num){
             factors.push(i)
         }
     }
+    if (factors.length === 0){
+        return "NO"
+    }
     totalSum = factors.reduce((prev, curr)=>prev + curr)
-    console.log(totalSum)
+    if(totalSum==num){
+        return "YES"
+    }
+    else{
+        return "NO"
+    }
 }
 
-var func = function func(){
-    console.log(func === func)
+function main(input) {
+    var result =[]
+    var inputArray = input.split("\n")
+    var testCases = inputArray.shift()
+
+    for(let i = 0; i < testCases; i++){
+        let temp = check(inputArray[i])
+        result.push(temp)
+    }
+    var ans = result.join("\n")
+    
+    process.stdout.write(ans);       // Writing output to STDOUT
 }
-func()
